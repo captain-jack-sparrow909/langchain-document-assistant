1cce9dfa286f27cb73ae2ede9a1931d2c4a9a8de
Captain Jack Sparrow <15pwele4721@uetpeshawar.edu.pk>
2026-06-09 20:24:55 +0400

    setting env

---
+++ ingestion.py
@@ -0,0 +1,40 @@
+import os
+import ssl
+import certifi
+
+from dotenv import load_dotenv
+load_dotenv()
+from langchain_openai import OpenAIEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+# CharacterTextSplitter splitter uses a single separator (or a list of separators in a straightforward way) to split text into chunks of a target size.
+# RecursiveCharacterTextSplitter -> Instead of relying on a single separator, it tries a hierarchy of separators, recursively splitting until chunks fit within the desired size.
+from langchain_core.documents import Document
+from langchain_pinecone import PineconeVectorStore
+from langchain_openai import OpenAIEmbeddings
+from langchain_tavily import TavilyCrawl, TavilyExtract, TavilyMap
+
+from logger import (Colors, log_error, log_header, log_info, log_success, log_warning)
+
+
+# configure SSL context to use certifi certificates, this is needed because we're going to use 
+# TavilyCrawl to scrape websites and it uses requests library which doesn't 
+# use the system's CA bundle by default.
+
+ssl_context = ssl.create_default_context(cafile=certifi.where())
+os.environ["SSL_CERT_FILE "] = certifi.where()
+os.environ[" REQUESTS_CA_BUNDLE"] = certifi.where()
+
+
+# embeddings:
+embeddings = OpenAIEmbeddings(model="text-embedding-3-small", show_progress_bar=False, chunk_size=50, retry_max_seconds=10)
+
+# vector store:
+vector_store = PineconeVectorStore(index_name=os.environ["PINECONE_INDEX_NAME"], embeddings=embeddings)
+
+# tavily: below is to help an LLM search, explore, and extract structured content from the web.
+tavily_extract = TavilyExtract() 
+tavily_map = TavilyMap(max_depth=5, max_breadth=20, max_pages=1000)
+tavily_crawl = TavilyCrawl()
+
+
+
